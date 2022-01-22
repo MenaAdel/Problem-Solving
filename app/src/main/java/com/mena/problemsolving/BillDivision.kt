5987556a548f0577a9720af902152dec924f3515
m.adel <m.adel@t2.sa>
2022-01-22 20:00:44 +0200

    Add BillDivision

---
+++ app/src/main/java/com/mena/problemsolving/BillDivision.kt
@@ -0,0 +1,40 @@
+package com.mena.problemsolving
+
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
+
+fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+    var sum = 0
+    bill.filterIndexed { index, i ->
+        index != k
+    }.forEach {
+        sum += it
+    }
+    var actual = sum.div(2)
+    if (actual == b){
+        println("Bon Appetit")
+    } else {
+        println(Math.abs(b - actual))
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val bill = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trim().toInt()
+
+    bonAppetit(bill, k, b)
+}
